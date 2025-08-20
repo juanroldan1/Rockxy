7bb2eba2012e4bb308e2710e00b037ca0c950ea8
juanroldan1 <juansanrol@unisabana.edu.co>
2025-08-20 16:36:39 -0500

    Js

---
+++ funciones/funciones.js
@@ -0,0 +1,15 @@
+//Dom
+document.addEventListener('DOMContentLoaded',function(){
+    const agregarCarrito = this.getElementsByClassName("AñadirCarrito");
+
+
+
+    //Variables de estado
+    let productosAñadidosCarrito = [];
+
+    //Funcion para añadir elementos al carrito
+    function AñadirCarrito(){
+        agregarCarrito.innerHTML = AñadirCarrito.slice(-10).join('<br>');
+    }
+
+});
